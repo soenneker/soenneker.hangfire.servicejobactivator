784744ad508f8e3ee7ac88eb1468dca68b9bd401
Jake Soenneker <jake@soenneker.com>
2025-02-11 12:53:18 -0600

    Fluent

--- src/Registrars/ServiceJobActivatorRegistrar.cs
+++ src/Registrars/ServiceJobActivatorRegistrar.cs
@@ -12,8 +12,10 @@ public static class ServiceJobActivatorRegistrar
     /// <summary>
     /// Overrides the default Hangfire activator and resolves services through .NET's default DI provider
     /// </summary>
-    public static void AddServiceJobActivator(this IGlobalConfiguration config, IServiceProvider serviceProvider)
+    public static IServiceProvider AddServiceJobActivator(this IGlobalConfiguration config, IServiceProvider services)
     {
-        config.UseActivator(new ServiceJobActivator(serviceProvider.GetService<IServiceScopeFactory>()!));
+        config.UseActivator(new ServiceJobActivator(services.GetService<IServiceScopeFactory>()!));
+
+        return services;
     }
 }
