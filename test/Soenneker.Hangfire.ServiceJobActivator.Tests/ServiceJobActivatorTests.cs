5bfadc0b1fc1bf801cb9d834327b8e2fc1e5bbda
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:12 -0500

    TUnit conversion

--- test/Soenneker.Hangfire.ServiceJobActivator.Tests/ServiceJobActivatorTests.cs
+++ test/Soenneker.Hangfire.ServiceJobActivator.Tests/ServiceJobActivatorTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Hangfire.ServiceJobActivator.Tests;
 
-[Collection("Collection")]
-public class ServiceJobActivatorTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class ServiceJobActivatorTests : HostedUnitTest
 {
-    public ServiceJobActivatorTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public ServiceJobActivatorTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
     }
