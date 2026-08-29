c62c52ee6487ab2b1d064a4bd6ec7e42bd3deae3
soenneker <jake@soenneker.com>
2026-08-29 14:17:06 -0500

    Improve public API XML documentation

--- src/Soenneker.Hangfire.ServiceJobActivator/Registrars/ServiceJobActivatorRegistrar.cs
+++ src/Soenneker.Hangfire.ServiceJobActivator/Registrars/ServiceJobActivatorRegistrar.cs
@@ -12,6 +12,9 @@ public static class ServiceJobActivatorRegistrar
     /// <summary>
     /// Overrides the default Hangfire activator and resolves services through .NET's default DI provider
     /// </summary>
+    /// <param name="config">Config for the add service job activator operation.</param>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The resulting global Configuration.</returns>
     public static IGlobalConfiguration AddServiceJobActivator(this IGlobalConfiguration config, IServiceProvider services)
     {
         config.UseActivator(new ServiceJobActivator(services.GetService<IServiceScopeFactory>()!));
